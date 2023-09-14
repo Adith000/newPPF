24779b1617e3b77b4747714e6779e553c21edd2e
Adith000 <97776844+Adith000@users.noreply.github.com>
2023-09-14 21:57:38 +0530

    Update app.js
    changing App.js to see whether dirname functions

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const  express =require("express") ;
 const  dotenv =require("dotenv") ;
 const  path =require("path") ;
-
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app =express();
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname( __filename);
 
  //Portfolio\dist
 app.use(express.static(path.join( __dirname,'./Portfolio/dist')))
